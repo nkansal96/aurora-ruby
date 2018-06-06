f55e4cd42e443afaaa59d718bd3efdbbfc1f7a56
Daniel Lee <danieldavidlee@hotmail.com>
2018-06-05 20:25:27 -0700

    Added option to specify portaudio binary path via environment variable

--- lib/portaudio/helpers.rb
+++ lib/portaudio/helpers.rb
@@ -1,6 +1,10 @@
 module Portaudio
     # Decide which libary to load based on OS
     def self.get_lib
+        if ENV.has_key?("PA_PATH") && File.exist?(File.expand_path(ENV["PA_PATH"]))
+            return File.expand_path(ENV["PA_PATH"])
+        end
+
         if Gem::Platform.local.os == "mingw32" && Gem::Platform.local.cpu == "x64"
             return File.absolute_path("lib/portaudio/binaries/libportaudio64bit.dll")
         elsif Gem::Platform.local.os == "mingw32" && Gem::Platform.local.cpu == "x86"
